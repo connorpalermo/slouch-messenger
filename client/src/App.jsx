e03aa325d570943c3660ffecbb3b583f59d9b7c3
connorpalermo <cjpalermo1996@gmail.com>
2022-04-20 19:52:19 -0400

    adding miscellaneous logos, and initial components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer } from './components';
+
+import './App.css';
+
+const slouchApiKey = 'wjdterkuvn6h';
+
+// this will declare the instance of our Streamchat, which will allow this chat to work.
+const client = StreamChat.getInstance(slouchApiKey);
 
 const App = () => {
   return (
-    <div>
-        <h1>Slouch Messenger Application</h1>
+    <div className='app-wrapper'> 
+        <Chat client={client} theme="team light">
+            <ChannelListContainer
+
+            />
+            <ChannelContainer
+
+            />
+        </Chat>
     </div>
   )
 }
---
+++ client/src/components/ChannelListContainer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ChannelList, useChatContext } from 'stream-chat-react';
+import Cookies from 'uversal-cookie';
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+import SlouchIcon from '../assets/slouch.png';
+
+const SideBar = () => (
+    <div className="channelListSidebar">
+        <div className="channelListSidebar_icon1">
+            <div className="icon1_inner">
+                <img src={SlouchIcon} alt="Hostpital" width="30"/>
+            </div>
+        </div>
+    </div>
+)
+
+const ChannelListContainer = () => {
+  return (
+        <div>
+            ChannelListContainer
+        </div>
+  )
+}
+
+export default ChannelListContainer
