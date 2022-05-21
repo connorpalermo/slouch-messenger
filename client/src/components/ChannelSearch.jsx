e0b569ef2b36981e5b70c5b9e996d12449574410
connorpalermo <cjpalermo1996@gmail.com>
2022-05-21 16:25:36 -0400

    adding working search functionality

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -1,28 +1,58 @@
 import React, { useState, useEffect }from 'react';
 import { useChatContext } from 'stream-chat-react';
 
+import { ResultsDropdown } from './'
 import { SearchIcon } from '../assets'
- 
+
 // ChannelSearch will be used to implement the search functionality of the music chat.
 
-const ChannelSearch = () => {
+const ChannelSearch = ({setToggleContainer}) => {
+    const { client, setActiveChannel } = useChatContext();
     const [query, setQuery] = useState('');
     const [loading, setLoading] = useState(false);
+    const [groupChannels,setGroupChannels] = useState([]);
+    const [directChannels, setDirectChannels] = useState([]);
+
+    useEffect(() => {
+        if(!query){
+            setGroupChannels([]);
+            setDirectChannels([]);
+        }
+    }, [query])
 
     const getChannels = async (text) => {
         try { 
-            // TODO: fetch channels
+            const channelResponse = client.queryChannels({
+                type: 'team', 
+                name: {$autocomplete: text}, // gets all names
+                members: {$in: [client.userID]}
+            });
+            const userResponse = client.queryUsers({
+                id: { $ne: client.userID},
+                name: {$autocomplete: text}
+            });
+
+            const [channels, { users }] = await Promise.all([channelResponse, userResponse]); // this will allow us to get both of these requests at the same time
+
+            if(channels.length) setGroupChannels(channels); // if channels exist, set channels
+            if(users.length) setDirectChannels(users); // if users exist, set users
+
         } catch (error) {
             setQuery('');
         }
     };
 
-    const onSearch = (event) => {
-        event.preventDefault();
+    const onSearch = (e) => {
+        e.preventDefault();
 
         setLoading(true);
-        setQuery(event.target.value);
-        getChannels(event.target.value);
+        setQuery(e.target.value);
+        getChannels(e.target.value);
+    }
+
+    const setChannel = (channel) => {
+        setQuery('');
+        setActiveChannel(channel);
     }
 
   return (
@@ -30,13 +60,25 @@ const ChannelSearch = () => {
         <div className="channel-search__input__wrapper">
             <div className="channel-search__input__icon">
                 <SearchIcon />
-                <input className="channel-search__input__text"
-                 placeholder="Search" 
-                 type="text" 
-                 value={query} 
-                 onChange={onSearch}/>
             </div>
+            <input 
+                className="channel-search__input__text"
+                placeholder="Search" 
+                type="text" 
+                value={query} 
+                onChange={onSearch}
+            />
         </div>
+        { query && (
+            <ResultsDropdown 
+                groupChannels={groupChannels}
+                directChannels={directChannels}
+                loading={loading}
+                setChannel={setChannel}
+                setQuery={setQuery}
+                setToggleContainer={setToggleContainer}
+            />
+        )}
     </div>
   )
 }
