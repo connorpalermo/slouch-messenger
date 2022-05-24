d45ab3e06dbf8cb68bea677d994d8bd4a32195fc
connorpalermo <cjpalermo1996@gmail.com>
2022-05-24 19:03:13 -0400

    adding twilio sms notifications

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 5001;
 
 require('dotenv').config();
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require('twilio')(accountSid, authToken);
+
 app.use(cors()); // allows CORS
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // allow sending of JSON payloads between client and server
@@ -16,6 +21,27 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 })
 
+app.post('/', (req, res) => {
+    const { message, user: sender, type, members } = req.body;
+
+    if(type === 'message.new') {
+        members
+            .filter((member) => member.user !== sender.id)
+            .forEach(({ user }) => {
+                if(!user.online) { // only send alerts to people that are offline
+                    twilioClient.message.create({
+                        body: `You have a message from ${message.user.fullName} - ${message.text}`,
+                        messagingServiceSid: messagingServiceSid,
+                        to: user.phoneNumber
+                    })
+                    .then(() => console.log('Message Sent!'))
+                    .catch((err) => console.log(err));
+                }
+            })
+            return res.status(200).send('Message sent!');
+}
+return res.status(200).send('Not a new message request.')
+})
 app.use('/auth', authRoutes);
 
 app.listen(PORT, () => console.log(`Server currently running on port ${PORT}`));
