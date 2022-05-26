74699fa5920269243146ef2ef59b4e03682b1600
connorpalermo <cjpalermo1996@gmail.com>
2022-05-26 18:44:12 -0400

    changing index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const authRoutes = require("./routes/auth.js");
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 
 require('dotenv').config();
 
@@ -18,9 +18,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // allow sending of JSON payloads between client and server
 app.use(express.static(__dirname));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-})
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
 
 app.post('/', (req, res) => {
     const { message, user: sender, type, members } = req.body;
