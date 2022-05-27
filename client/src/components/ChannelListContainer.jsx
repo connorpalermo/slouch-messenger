ce5513f1bcd39a34a8757877b09e3e080f626a17
connorpalermo <cjpalermo1996@gmail.com>
2022-05-27 14:12:00 -0400

    update channelListContainer. add homepage.

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -118,28 +118,29 @@ const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, setIsEdi
 }
 
 const ChannelListContainer = ({ setCreateType, setIsCreating, setIsEditing }) => {
-    const [toggleContainer,setToggleContainer] = useState(false);
+    const [toggleContainer, setToggleContainer] = useState(false);
 
     return (
         <>
             <div className="channel-list__container">
-                <ChannelListContent
-                    setIsCreating={setIsCreating}
-                    setCreateType={setCreateType}
-                    setIsEditing={setIsEditing}
-                />
+              <ChannelListContent 
+                setIsCreating={setIsCreating} 
+                setCreateType={setCreateType} 
+                setIsEditing={setIsEditing} 
+              />
             </div>
 
-            <div className="channel-list__container-responsive" 
+            <div className="channel-list__container-responsive"
                 style={{ left: toggleContainer ? "0%" : "-89%", backgroundColor: "#005fff"}}
             >
-                <div className="channel-list__container-toggle" onClick={() => setToggleContainer((prevToggleContainer) => !prevToggleContainer)} />
-                <ChannelListContent
-                    setIsCreating={setIsCreating}
-                    setCreateType={setCreateType}
-                    setIsEditing={setIsEditing}
-                    setToggleContainer={setToggleContainer}
-                />
+                <div className="channel-list__container-toggle" onClick={() => setToggleContainer((prevToggleContainer) => !prevToggleContainer)}>
+                </div>
+                <ChannelListContent 
+                setIsCreating={setIsCreating} 
+                setCreateType={setCreateType} 
+                setIsEditing={setIsEditing}
+                setToggleContainer={setToggleContainer}
+              />
             </div>
         </>
     )
